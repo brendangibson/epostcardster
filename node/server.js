06145338fac7928bedd50a71329a577f93d2883b
Brendan Gibson <brendan@area1security.com>
2016-03-05 21:42:31 -0800

    Added db

--- node/server.js
+++ node/server.js
@@ -80,7 +80,19 @@ handleRequest = function (req, res) {
 	}
 };
 
+var pg = require('pg');
 
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 http.createServer(handleRequest).listen(process.env.PORT || 8888);
 
